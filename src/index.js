bbd1367a44e0ba88db3878802400220a2af40851
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2020-05-23 11:09:43 -0300

    alternando entre 'X' e 'O'

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         }
     }
 
@@ -27,8 +28,11 @@ class Board extends React.Component {
          * Evitar mutação nos permite manter o histórico das versões anteriores do jogo intacta e reutiliza-las mais tarde.
          */
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     renderSquare(i) {
@@ -41,7 +45,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
         return (
         <div>
