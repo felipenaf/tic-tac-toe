c7cce41b64707e7d1f3fef9b722c589c2ce3954f
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2020-05-25 10:34:01 -0300

    Identificação das colunas e linhas

--- src/board.js
+++ src/board.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Square} from './square.js';
+import {SquareId} from './squareId.js';
 
 class Board extends React.Component {
     renderSquare(i) {
@@ -11,20 +12,37 @@ class Board extends React.Component {
         );
     }
 
+    renderSquareId(i) {
+        return (
+            <SquareId
+                value={i}
+            />
+        );
+    }
+
     render() {
         return (
             <div>
                 <div className="board-row">
+                    {this.renderSquareId()}
+                    {this.renderSquareId('1')}
+                    {this.renderSquareId('2')}
+                    {this.renderSquareId('3')}
+                </div>
+                <div className="board-row">
+                    {this.renderSquareId('1')}
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                 </div>
                 <div className="board-row">
+                    {this.renderSquareId('2')}
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
                 </div>
                 <div className="board-row">
+                    {this.renderSquareId('3')}
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
---
+++ src/squareId.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SquareId(props) {
+    return (
+        <button className="square-id">
+            {props.value}
+        </button>
+    );
+}
