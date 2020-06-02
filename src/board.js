3fc5084b1261d8822df6e2ec64d323cfb40e382c
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2020-06-02 17:49:49 -0300

    Reescrever o componente Board para utilizar 2 loops para fazer os quadrados, em vez de deixá-los hardcoded.

--- src/board.js
+++ src/board.js
@@ -1,52 +1,48 @@
 import React from 'react';
 import {Square} from './square.js';
-import {SquareId} from './squareId.js';
 
 class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
+                key={i}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
             />
         );
     }
 
-    renderSquareId(i) {
-        return (
-            <SquareId
-                value={i}
-            />
-        );
-    }
-
+    /**
+     * Todos os elementos que entram na iteração precisam de "key"
+     * Tanto o component Square como a div em que ele se encontra
+     */
     render() {
+        const cols = Array(3).fill(null);
+        const rows = Array(3).fill(null);
+        const board = [];
+        let iterator = 0;
+
+        rows.map(() => {
+            const itens = [];
+
+            cols.map(() => {
+                itens.push(
+                    this.renderSquare(iterator)
+                );
+
+                iterator++;
+            });
+
+            board.push(
+                <div key={iterator} className="board-row">
+                    {itens.map((x) => x)}
+                </div>
+            );
+        });
+
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquareId()}
-                    {this.renderSquareId('1')}
-                    {this.renderSquareId('2')}
-                    {this.renderSquareId('3')}
-                </div>
-                <div className="board-row">
-                    {this.renderSquareId('1')}
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquareId('2')}
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquareId('3')}
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {board}
             </div>
         );
     }
